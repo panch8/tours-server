3265776fadd373938a468c4d96a22f7096235821
panch8 <98569130+panch8@users.noreply.github.com>
2023-10-18 11:10:37 +0200

    fix bug

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
+
 
 
 const router = express.Router();
 
 
 router.get('/',
-bookingController.createBookingCheckout,
+// bookingController.createBookingCheckout,
 authController.isLoggedIn, 
 viewController.getOverview);
 router.get('/tour/:tourSlug',authController.isLoggedIn, viewController.getTour);
